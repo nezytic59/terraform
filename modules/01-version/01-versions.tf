3623989c3bb03b2ff6822fec8f5549a71cb5f0fb
nezytic59 <tmdgus951116@gmail.com>
2023-03-09 17:17:43 +0900

    23/03/09

---
+++ modules/01-version/01-versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      # version = "~> 3.0"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region  
+  default_tags {
+    tags = {
+      Terraform = "true"
+      Project   = "${var.cluster_name}-project"
+    }
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+aws_region        = "ap-northeast-2"
+environment       = "dev"
+business_division = "it"
